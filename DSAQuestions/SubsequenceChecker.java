f21b57637c9b445a3ff922598e252dd6a49f287b
Rishu <500061292@upesalumni.upes.ac.in>
2023-07-10 02:02:05 +0530

    Fixed issue in Subsequence Code.

--- DSAQuestions/SubsequenceChecker.java
+++ DSAQuestions/SubsequenceChecker.java
@@ -1,57 +1,40 @@
 
 import java.util.*;
 
-public class SubsequenceChecker
-{
-    public static boolean isSubsequence(String s, String t)
-    {
-        int sLength = s.length();
-        int tLength = t.length();
-        int sIndex  = 0;
-        int tIndex  = 0;
-
-        while (sIndex < sLength && tIndex < tLength){
-            if (s.charAt(sIndex) == t.charAt(tIndex)){
-                sIndex++;
-            }
-            tIndex++;
-        }
-        return sIndex == sLength;
-    }
-
-    //Using HashMap
-    public static boolean isSubsequenceHashMap(String s, String t)
-    {
+public class SubsequenceChecker {
+    public static boolean isSubsequence(String s, String t) {
         Map<Character, List<Integer>> indexMap = new HashMap<>();
-
-        for (int i = 0; i < t.length(); i++){
+        for (int i = 0; i < t.length(); i++) {
             char c = t.charAt(i);
             indexMap.computeIfAbsent(c, key -> new ArrayList<>()).add(i);
         }
-
+        
         int prevIndex = -1;
-
-        //Iterate through s and perform constant time lookup s in the indexMap.
-        for (char c : s.toCharArray()){
-            if (!indexMap.containsKey(c)){
+        for (char c : s.toCharArray()) {
+            if (!indexMap.containsKey(c)) {
                 return false;
             }
+            
             List<Integer> indices = indexMap.get(c);
-            int insertionIndex = Collections.binarySearch(indices, prevIndex+1);
-
-            if (insertionIndex < 0){ insertionIndex = -(insertionIndex + 1);}
-            if (insertionIndex == indices.size()){ return false; }
+            int insertionIndex = Collections.binarySearch(indices, prevIndex + 1);
+            if (insertionIndex < 0) {
+                insertionIndex = -(insertionIndex + 1);
+            }
+            
+            if (insertionIndex == indices.size() || indices.get(insertionIndex) <= prevIndex) {
+                return false;
+            }
+            
             prevIndex = indices.get(insertionIndex);
         }
+        
         return true;
     }
 
-
-    //Driver Code
-    public static void main(String[] args){
-        String s = "abc";
-        String t = "ahbgce";
-        boolean result = isSubsequenceHashMap(s, t);
+    public static void main(String[] args) {
+        String s = "ab";
+        String t = "baab";
+        boolean result = isSubsequence(s, t);
         System.out.println(result);
     }
 }
--- DSAQuestions/SubsequenceChecker_python.py
+++ DSAQuestions/SubsequenceChecker_python.py
@@ -1,41 +1,32 @@
 import bisect
 
-def is_subsequence(s, t):
-    s_length = len(s)
-    t_length = len(t)
-    s_index  = 0
-    t_index  = 0
+class SubsequenceChecker_python:
 
-    while s_index < s_length and t_index < t_length:
-        if s[s_index] == t[t_index]:
-            s_index += 1
-        t_index += 1
-    return s_index == s_index
+    def isSubsequence(self, s: str, t: str) -> bool:
+        index_map = {}
 
-def is_subsequence_hash_map(s, t):
-    index_map = {}
+        for i, c in enumerate(t):
+            if c not in index_map:
+                index_map[c] = []
+            index_map[c].append(i)
 
-    for i, c in enumerate(t):
-        if c not in index_map:
-            index_map[c] = []
-        index_map[c].append(i)
+        prev_index = -1
 
-    prev_index = -1
+        for c in s:
+            if c not in index_map:
+                return False
+            indices = index_map[c]
+            insertion_index = bisect.bisect_left(indices, prev_index + 1)
 
-    for c in s:
-        if c not in index_map:
-            return False
-        indices = index_map[c]
-        insertion_index = bisect.bisect_left(indices, prev_index+1)
-
-        if insertion_index == len(indices):
-            return False
-        prev_index = indices[insertion_index]
-    return True
+            if insertion_index == len(indices):
+                return False
+            prev_index = indices[insertion_index]
+        return True
 
 #Driver Code
-s = "abc"
-t = "ahbgce"
-result = is_subsequence_hash_map(s, t)
+s = "ab"
+t = "baab"
+solution = SubsequenceChecker_python()
+result = solution.isSubsequence(s, t)
 print(result)        
 
