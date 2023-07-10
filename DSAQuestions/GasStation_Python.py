94d72cf97605f7ca91bbd48a6d4912843318dc0a
Rishu <500061292@upesalumni.upes.ac.in>
2023-07-11 00:12:01 +0530

    10July modifications.

--- DSAQuestions/GasStation_Python.py
+++ DSAQuestions/GasStation_Python.py
@@ -1,20 +1,25 @@
-def can_complete_circuit(gas, cost):
-    n = len(gas)
-    start = 0
-    total = 0
-    tank = 0
-    for i in range(n):
-        tank += gas[i] - cost[i]
-        total += gas[i] - cost[i]
+class Solution:
+    def can_complete_circuit(self, gas, cost):
+        n = len(gas)
+        start = 0
+        total = 0
+        tank = 0
 
-        if tank < 0 :
-            start = i+1
-            tank = 0
-            
-    return start if total >= 0 else -1
+        for i  in range(n):
+            tank += gas[i] - cost[i]
+            total += gas[i] - cost[i]
+
+            if tank < 0:
+                start = i+1
+                tank = 0
+
+        return start if total >= 0 else -1        
+
 
 # Driver code
 gas = [1,2,3,4,5]
 cost = [3,4,5,1,2]
-result = can_complete_circuit(gas, cost)
-print("Starting gas station index : ", result)
+
+solution = Solution()
+result = solution.can_complete_circuit(gas, cost)
+print("Starting gas station index:", result)
